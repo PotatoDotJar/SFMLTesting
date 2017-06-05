114649c6be0a17d387e759081c99551c85cdd6f0
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-06-05 00:34:15 -0400

    Added some shape arrays and math functions.

--- SFMLTesting/Main.cpp
+++ SFMLTesting/Main.cpp
@@ -3,17 +3,32 @@ Main.cpp
 Author: PotatoSauceVFX - 2017
 */
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
 
 using namespace sf;
+using namespace std;
 
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
-int main()
-{
-	RenderWindow window(VideoMode(WIDTH, HEIGHT), "SFML works!");
-	CircleShape shape(100.f);
-	shape.setFillColor(Color::Green);
+Vector2i mousePos = Mouse::getPosition();
+
+float yOffset;
+
+int main() {
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "My Window");
+	window.setFramerateLimit(60.0);
+
+	CircleShape shapes[WIDTH];
+
+	for (int i = 0; i < (sizeof(shapes) / sizeof(*shapes)); i++) {
+		shapes[i] = CircleShape(1.0f);
+	}
+	for (int i = 0; i < (sizeof(shapes) / sizeof(*shapes)); i++) {
+		shapes[i].setFillColor(Color(0, 233, 0, 255));
+	}
 
 	while (window.isOpen())
 	{
@@ -24,8 +39,23 @@ int main()
 				window.close();
 		}
 
+		// Make yPos calculations
+		
+
+		// Setting positions
+		for (int i = 0; i < (sizeof(shapes) / sizeof(*shapes)); i++) {
+			yOffset = (sin(0.03*i))*(HEIGHT / 3);
+			shapes[i].setPosition(i, ((HEIGHT / 2.0) - 5.0) + yOffset);
+		}
+		//shape.setPosition(mouseX -5.0, ((HEIGHT / 2.0)-5.0)+yOffset);
+		//shape2.setPosition(mouseX - 5.0, ((HEIGHT / 2.0) - 5.0));
+
+
+		
 		window.clear();
-		window.draw(shape);
+		for (int i = 0; i < (sizeof(shapes) / sizeof(*shapes)); i++) {
+			window.draw(shapes[i]);
+		}
 		window.display();
 	}
 
